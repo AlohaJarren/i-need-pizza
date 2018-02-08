49a8c63dd78e1497faf820782fa8b05afd270ea4
AlohaJarren <calizo.jarren@gmail.com>
2018-02-07 14:39:34 -1000

    Almost Done

--- js/pizza.js
+++ js/pizza.js
@@ -5,14 +5,62 @@
 * Keys with more than one value should be an array of strings ex. Veggies
 */
 
+var order11821 = {
+  customerName: "James Franko",
+  address: "2155 E Lake Dr",
+  city: "Seattle",
+  state: "WA",
+  zipCode: "11310",
+  pizza: {  crust: "thin", pizzaSauce: "robust marinara", cheese: "regular",
+      toppings: {
+              veggies: ["olives", "green peppers"],
+              meats: ["pepperoni", "ham", "sausage"]
+            }
+    },
+  sideItems: ["breadsticks", "bbq wings 6 piece"],
+  sauces: "ranch",
+  specialInstructions: "please cut into squares",
+  paymentType: "cash",
+  storeId: 112
+}
 
+var order11372 = {
+  customerName: "Justin Bieber",
+  address: "1801 W Rodeo Dr",
+  city: "Beverly Hills",
+  state: "CA",
+  zipCode: "91833",
+  pizza: {
+    crust: "stuffed crust",
+    pizzaSauce: "creamy garlic alfredo",
+    cheese: "double",
+    toppings: {
+      veggies: ["onion", "anchovies", "mushrooms"],
+      meat: ["chicken", "bacon"]
+    }
+  },
+  sideItems: "doube fudge brownie dessert",
+  sauces: ["bbq", "garlic sauce"],
+  specialInstructions: "call 815 at the gate",
+  paymentType: "credit card",
+  storeId: 382
+}
 /*
 * Create a new variable named currentOrders
 * currentOrders should be single object containing both orders from above
 */
+
+var currentOrders = {firstOrder: "order11872", secondOrder: "order11372"}
 
 /*
 * Make the changes to the currentOrders object based on the ReadMe
 * Run your file with node pizza.js
 * Console.log(currentOrders) to verify changes have been made
 */
+
+currentOrders.order11821.pizza.pizzaSauce["pizzaSauce"] = "classic marinara";
+currentOrders.order11821.pizza.toppings.veggies = ["onion", "anchovies", "mushrooms", "spinach"];
+currentOrders.order11821.pizza.toppings.meat = ["pepperoni", "ham"];
+currentOrders.order11821.orderTime = "20 minutes";
+
+console.log(currentOrders());
